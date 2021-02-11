4da8fa30d6744d603f60a191a98e3403b807d11c
canguruhh <sven@mutzl.org>
2021-02-11 21:16:22 +0100

    add spinner component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
-import { CounterComponent } from './components/counter/counter.component';
-import { AccountComponent } from './components/account/account.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component'
+import { CounterComponent } from './components/counter/counter.component'
+import { AccountComponent } from './components/account/account.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import { CounterIndicatorComponent } from './components/counter-indicator/counter-indicator.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { CounterIndicatorComponent } from './components/counter-indicator/counter-indicator.component'
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { CounterIndicatorComponent } from './components/counter-indicator/counte
     MatCardModule,
     MatSnackBarModule,
     BrowserAnimationsModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
